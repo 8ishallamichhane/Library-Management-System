3979af394f64f5d198c0e68b180b6fb45f47f8e1
Bishal Lamichhane <91371293+Bishal-Lc@users.noreply.github.com>
2023-08-09 08:22:59 +0545

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                  <div class="tag1"> &copy; <?php echo date('Y');?> RSS BookTown|Library Management System </div><a href="https://bishal-lamichhane.com.np/" target="_blank"  ><div class="tag"> Designed by : Bishal Lamichhane</div></a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
